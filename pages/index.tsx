d7a2e1537a983a93f39c07fe14c05a900d55b0d7
Ilya Daraseliya <idarase@gmail.com>
2020-07-11 19:11:38 +0200

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,19 +40,8 @@ const Home = (props: Data) => {
               readymade gluten
             </h1>
             <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
+              {props.homePage.introductionText}
             </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-primary-500 border-0 py-2 px-6 focus:outline-none hover:bg-primary-600 rounded text-lg">
-                Button
-              </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">
-                Button
-              </button>
-            </div>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
             <UndrawWorkout primaryColor={props.site.color} />
